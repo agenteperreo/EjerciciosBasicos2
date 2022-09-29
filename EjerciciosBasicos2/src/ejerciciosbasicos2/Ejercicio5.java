2b019eb74dafe59a6fb1d1204e8b0c87415325a7
igarcia <igarcia@106-10.info2>
2022-09-29 13:07:03 +0200

    29/9/2022

--- EjerciciosBasicos2/src/ejerciciosbasicos2/Ejercicio5.java
+++ EjerciciosBasicos2/src/ejerciciosbasicos2/Ejercicio5.java
@@ -8,23 +8,22 @@ public class Ejercicio5 {
 
 		// Abrimos el escanner y declaramos las variables
 		Scanner sc = new Scanner(System.in);
-		int mediaE;
-		float nota1, nota2, nota3, mediaF;
+		int nota1, nota2, nota3;
+		double notaMedia;
 
 		// Guardamos las 3 notas en las variables correspondientes
 		System.out.print("Dime la nota del primer trimestre: ");
-		nota1 = sc.nextFloat();
+		nota1 = sc.nextInt();
 		System.out.print("Dime la nota del segundo trimestre: ");
-		nota2 = sc.nextFloat();
+		nota2 = sc.nextInt();
 		System.out.print("Dime la nota del tercer trimestre: ");
-		nota3 = sc.nextFloat();
+		nota3 = sc.nextInt();
 
 		// Calculamos la media con decimales y sin decimales
-		mediaE = (int) ((nota1 + nota2 + nota3) / 3);
-		mediaF = (nota1 + nota2 + nota3) / 3;
+		notaMedia = (double)(nota1 + nota2 + nota3) / 3;
 
 		// Mostramos las notas medias
-		System.out.printf("La nota media con decimales es %.2f y la nota del boletin de notas es %d", mediaF, mediaE);
+		System.out.printf("La nota media con decimales es %.2f y entera es %.2f", notaMedia, (int)notaMedia);
 
 		// Cerramose el escaner
 		sc.close();
